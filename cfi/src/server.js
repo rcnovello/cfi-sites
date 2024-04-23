23a007e8d37636faab4f95d70cf78279e0adf606
rcnovello <ronnicorrea@hotmail.com>
2024-04-22 21:41:03 -0300

    teste nginx

--- cfi/src/server.js
+++ cfi/src/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const bodyParser = require('body-parser'); 
 const app = express();
 //const os = require('os');
-const dlcHelpers = require("./helpers");
+//const dlcHelpers = require("./helpers");
 //const vConsts = require("./consts");
 //const fs = require('fs'); 
 
@@ -19,12 +19,14 @@ if (!vConsts.funcCheckServer() == ''){
 
 //const __serverPortHttps = 3001; 
 //const __serverPortHttp = 3000;
+const port = 3000;
 
-const __serverPortHttp = 3000; 
 //const __serverIp = 'comofazerinformatica.com.br';
-const __serverIp = 'localhost';
+//const __serverIp = 'localhost';
+const hostname = 'localhost';
 
-const http = require('http');
+//const https = require('node:https');
+//const http = require('http');
 //const { Socket } = require('dgram');
 //const userInfo = os.userInfo();
 
@@ -47,9 +49,17 @@ app.set('views', path.join(__dirname, 'views')); // Use path.join para construir
 //** routes */
 require('./routes/app')(app);
 
-var server = http.createServer(app);
-server.listen(__serverPortHttp);    
+//var server = http.createServer(app);
 
+
+app.listen(port, () => {
+  console.log(`App(comofazerinformatica) sendo executado em: ${hostname}:${port}`);
+});
+
+
+/*
+server.listen(__serverPortHttp);    
+var server = http.createServer(app);
 
     server.on('listening', () => {
 
@@ -58,6 +68,20 @@ server.listen(__serverPortHttp);
     server.on('error', (error) => {
         console.log(`Server NOT running at: http://${__serverIp}:${__serverPortHttp}/${error}`);        
         dlcHelpers.writeLog(`Server NOT running at: http://${__serverIp}:${__serverPortHttp}/${error}`);                
+    });*/
+/*
+server.listen(__serverPortHttps);    
+var server = https.createServer(app);
+
+    server.on('listening', () => {
+
+        console.log(`App(comofazerinformatica) sendo executado em: ${__serverIp}:${__serverPortHttps}`);        
+    })   
+    server.on('error', (error) => {
+        console.log(`Server NOT running at: https://${__serverIp}:${__serverPortHttps}/${error}`);        
+        dlcHelpers.writeLog(`Server NOT running at: https://${__serverIp}:${__serverPortHttps}/${error}`);                
     });
+  */
+   // https.createServer(app).listen(8000); 
     
     
